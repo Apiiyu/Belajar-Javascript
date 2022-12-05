ed17e3a1cab11d7d34bfb42b343cf48fc4466d36
rafi.k <rafi.k@bts.id>
2022-12-05 11:10:32 +0700

    Learning: Data Types in Javascript

---
+++ 01 - Fundamentals/data-types.js
@@ -0,0 +1,21 @@
+let tryUndefinedDataType; // --> As you can see, this is statement. Becaus it does not have any value so javascript will be set data types tryUndefinedDataType as undefined.
+console.log(typeof (tryUndefinedDataType)); // --> It will be print undefined.
+
+let tryNumberDataType = 10; // --> And then for this, this is a statement and it has a value of number. So javascript will be set data types tryNumberDataType as number.
+console.log(typeof (tryNumberDataType)); // --> It will be print number.
+
+let tryStringDataType = "Hello World"; // --> And then for this, this is a statement and it has a value of string. So javascript will be set data types tryStringDataType as string.
+console.log(typeof (tryStringDataType)); // --> It will be print string.
+
+const answer = '"I think it\'s awesome!" - said the student'; // --> And for this, this is one of way to declare value if the value contains double single quote in one line.
+
+let doubleHelloWorld = tryStringDataType + tryStringDataType // --> This is one of way to concatenate string.
+
+console.log(`We'll try string interpolation, ${tryStringDataType}`) // --> This is one of way to interpolate string.
+
+let booleanValue = true; // --> And then for this, this is a statement and it has a value of boolean. So javascript will be set data types booleanValue as boolean.
+console.log(typeof (booleanValue)); // --> It will be print boolean.
+
+let isGreater = 5 > 3; // --> Actually this expression is checking if 5 is greater than 3, So it will be return true (boolean value).
+
+let tryNullDataType = null; // --> And then for this, this is a statement and it has a value of null. So javascript will be set data types tryNullDataType as object.
