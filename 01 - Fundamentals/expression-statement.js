ed2d6b04c53405f9782d87195c1d9a6532cc9bb9
rafi.k <rafi.k@bts.id>
2022-12-05 10:04:13 +0700

    Initial Commit

---
+++ 01 - Fundamentals/expression-statement.js
@@ -0,0 +1,5 @@
+2 + 3 // --> As you can see, this is an expression. Expression is a piece of code that produces a value. 
+
+let userName; // --> And then for this, this is a statement. Statement is a piece of code that performs an action.
+
+// Path: 01 - Fundamentals\expression-statement.js
